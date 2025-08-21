88fa9af9fbe67c2b6580dfcf2f1661a47dc13f08
RasulFront <108578026+milfhntr@users.noreply.github.com>
2025-08-21 13:48:41 +0800

    Update HelloWorld.java

--- 01-Hello World/HelloWorld.java
+++ 01-Hello World/HelloWorld.java
@@ -1,12 +1,9 @@
-// Simple Java program
-// FileName: "HelloWorld.java"
-
-public class HelloWorld {
-    
-    // Your program begins with a call to main().
-    // Prints "Hello, World" to the terminal window.
-    public static void main(String[] args)
-    {
-        System.out.println("Hello, World");
-    }
+public class HelloWorld
+{
+	public static void main(String[] args) {
+		System.out.println("My name is Rasya");
+		System.out.println("Course name: OBJECT-ORIENTED PROGRAMMING IN JAVA I");
+		System.out.println("Course code: CSC200");
+		System.out.println("Lecturer name: Sathish Sarma");
+	}
 }
